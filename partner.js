171440af5250a726abfa360247851f775b6f3664
TomJohnes <79936338+TomJohnes@users.noreply.github.com>
2021-03-02 20:00:16 +0100

    Add files via upload

---
+++ partner.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'partner',
+    description: "Sendet den Einladungslink von Hirschies Server",
+    execute(message, args) {
+
+        message.channel.send('                              ')
+    }
+}
---
+++ twitch.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'twitch',
+    description: "Show twitch",
+    execute(message, args){
+        message.channel.send('                      ')
+    }
+}
